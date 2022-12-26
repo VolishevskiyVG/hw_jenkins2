abe4122b52bbc8782379f3f07a00e01d224074c5
Vasiliy Volishevskiy <Volishevski.V@gmail.com>
2022-12-26 18:17:04 +0300

    added video url

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -16,9 +16,9 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "100");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
