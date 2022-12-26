d669e5a06165ba5608ec73cbb15c59035efd4d59
Vasiliy Volishevskiy <Volishevski.V@gmail.com>
2022-12-26 18:02:22 +0300

    added video url

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -39,5 +40,9 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
+    }
+    @AfterAll
+    public static void afterAllTest(){
+
     }
 }
